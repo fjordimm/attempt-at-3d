5cbfb68d6d3afa547fb595fa87163b21d7ff4f9f
fjordimm <fjordimm@gmail.com>
2024-07-14 13:08:24 -0700

    got glew to work normally

--- AttemptAt3DProject/libs/AttemptAt3D/src/AttemptAt3D/Program.cpp
+++ AttemptAt3DProject/libs/AttemptAt3D/src/AttemptAt3D/Program.cpp
@@ -1,16 +1,73 @@
 
-#include "GLFW/glfw3.h"
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+const char* vertexShaderSource = R"glsl(
+	#version 150 core
+
+	in vec2 position;
+	in vec3 color;
+
+	out vec3 Color;
+
+	void main()
+	{
+		Color = color;
+		gl_Position = vec4(position, 0.0, 1.0);
+	}
+)glsl";
+
+const char* fragmentShaderSource = R"glsl(
+	#version 150 core
+
+	in vec3 Color;
+
+	out vec4 outColor;
+
+	void main()
+	{
+		outColor = vec4(Color, 1.0);
+	}
+)glsl";
+
+void checkShaderCompilation(GLuint shader)
+{
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
+	if (status == true)
+	{ return; }
+	else
+	{
+		static char buf[512];
+		glGetShaderInfoLog(shader, 512, nullptr, buf);
+		std::fprintf(stderr, "=== Shader did not compile ===\n");
+		std::fprintf(stderr, "%s\n", buf);
+		std::exit(EXIT_FAILURE);
+	}
+}
 
 int main(void)
 {
-    GLFWwindow* window;
+	std::ifstream ifs("MamaMia");
+	std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+	std::cout << "content..." << content << std::endl;
+	
+	GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(900, 480, "Hiiii this is my test window", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -20,19 +77,81 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	/* Initialize GLEW */
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	/////////////////////////////////////////////////
+		GLuint vao;
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		float verts1[] =
+		{
+			 0.0f,  0.5f,      1.0f,0.0f,0.0f,
+			 0.5f, -0.5f,      0.0f,1.0f,0.0f,
+			-0.5f, -0.5f,      0.0f,0.0f,1.0f
+		};
+
+		GLuint vbo;
+		glGenBuffers(1, &vbo);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verts1), verts1, GL_STATIC_DRAW);
+
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+		glCompileShader(vertexShader);
+		checkShaderCompilation(vertexShader);
+
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+		glCompileShader(fragmentShader);
+		checkShaderCompilation(fragmentShader);
+
+		GLuint shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+
+		glLinkProgram(shaderProgram);
+		glUseProgram(shaderProgram);
+
+		GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+		glEnableVertexAttribArray(posAttrib);
+		glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+
+		GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+		glEnableVertexAttribArray(colAttrib);
+		glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+	/////////////////////////////////////////////////
+
+	// GLuint vertexBuffer;
+	// glGenBuffers(1, &vertexBuffer);
+	// std::printf("%u\n", vertexBuffer);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+		/* Process inputs */
+
+        /* Render stuff */
+
 		glClearColor(0.1f, 0.0f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Do stuff required by GLFW */
+
+		glfwPollEvents();
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
     }
+
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
 
     glfwTerminate();
     return 0;
