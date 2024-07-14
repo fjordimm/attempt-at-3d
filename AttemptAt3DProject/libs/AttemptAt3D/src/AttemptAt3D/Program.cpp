2791055e8dd91568a432baf0c907afad27c157ac
fjordimm <fjordimm@gmail.com>
2024-07-14 13:04:10 -0700

    finally got glfw to work normally

--- AttemptAt3DProject/libs/AttemptAt3D/src/AttemptAt3D/Program.cpp
+++ AttemptAt3DProject/libs/AttemptAt3D/src/AttemptAt3D/Program.cpp
@@ -1,12 +1,39 @@
 
-#include <iostream>
-#include "AttemptAt3D/WindowHandling/Window.h"
-
-using AttemptAt3D::WindowHandling::Window;
+#include "GLFW/glfw3.h"
 
 int main(void)
 {
-	Window win;
+    GLFWwindow* window;
 
-	return 0;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+		glClearColor(0.1f, 0.0f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
