266736f2fe1817f1df0f49f958ae044df3f1415c
fjordimm <fjordimm@gmail.com>
2024-07-16 00:33:04 -0700

    got sun rotation to work

--- AttemptAt3DProject/libs/AttemptAt3D/res/shaders/fragmentShaderSource.glsl
+++ AttemptAt3DProject/libs/AttemptAt3D/res/shaders/fragmentShaderSource.glsl
@@ -7,13 +7,22 @@ out vec4 outColor;
 
 void main()
 {
-	float theta = radians(-90.0);
-	float cosTheta = cos(theta);
-	float sinTheta = sin(theta);
+	// float theta = radians(-90.0);
+	// float cosTheta = cos(theta);
+	// float sinTheta = sin(theta);
+	float alpha = radians(0.0);
+	float beta = radians(0.0);
+	float gamma = radians(90.0);
+	float cA = cos(alpha);
+	float sA = sin(alpha);
+	float cB = cos(beta);
+	float sB = sin(beta);
+	float cG = cos(gamma);
+	float sG = sin(gamma);
 	mat3 rotationMatrix;
-	rotationMatrix[0] = vec3(cosTheta, 0, sinTheta);
-	rotationMatrix[1] = vec3(0, 1, 0);
-	rotationMatrix[2] = vec3(-sinTheta, 0, cosTheta);
+	rotationMatrix[0] = vec3(cA*cB, cA*sB*sG-sA*cG, cA*sB*cG+sA*sG);
+	rotationMatrix[1] = vec3(sA*cB, sA*sB*sG+cA*cG, sA*sB*cG-cA*sG);
+	rotationMatrix[2] = vec3(-sG, cB*sG, cB*cG);
 
 	vec3 rotatedNormal = rotationMatrix * gNormal;
 
